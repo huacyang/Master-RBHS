236e0b5b119e7cdb05105bab3f59d16898a751b8
Sue Ann Ioanis <sioanis@me.com>
2013-04-03 15:30:38 -0400

    more stuff

--- includes/side_nav-information.php
+++ includes/side_nav-information.php
@@ -1,16 +1,16 @@
 <div id="side_nav" class="three pull-nine mobile-four columns"><br/>
 	<div class="wrapper">
     <!-- First Section -->
-    <h2>About</h2>
+    <h2>Information</h2>
     <div class="row">
         <div class="twelve columns sidenav-shift">
         <ul class="nav-bar vertical small">
-            <li><a href="#">Prospective Students</a></li>
+            <li><a href="information_prospective.php">Prospective Students</a></li>
             <li><a href="#">Current Students</a></li>
-            <li><a href="#">Patients</a></li>
-            <li><a href="#">Faculty/Staff</a></li>
-            <li><a href="#">Alumni</a></li>
-            <li><a href="#">Donors</a></li>
+            <li><a href="information_patients.php">Patients</a></li>
+            <li><a href="information_faculty_staff.php">Faculty/Staff</a></li>
+            <li><a href="information_alumni">Alumni</a></li>
+            <li><a href="information_donors">Donors</a></li>
         </ul>
         </div>
 	</div>
